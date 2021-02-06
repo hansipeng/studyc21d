36b57478171e95e201c0b8cc7f31448cadb9ed3e
hansipeng <hansipeng@126.com>
2021-02-06 18:27:37 -0500

    add firstat.c

---
+++ forstate.c
@@ -0,0 +1,17 @@
+/* Demostrates a simple for statement */
+
+#include <stdio.h>
+
+int count;
+
+int main(void)
+{
+    /* Print the numbers 1 throuth 20 */
+
+    for ( count = 1; count <= 20; count++ )
+    {
+        printf("%d\n", count);
+    }
+
+    return 0
+}
